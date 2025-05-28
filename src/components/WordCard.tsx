70bffca19d2978ef0143eee44baa1d41bb9f78d5
takakowatabe <kikotkk@gmail.com>
2025-05-28 18:39:08 +0900

    進捗

--- src/components/WordCard.tsx
+++ src/components/WordCard.tsx
@@ -10,6 +10,16 @@ type Props = {
 
 const WordCard = ({ word, onSave, isSaved = false }: Props) => {
 console.log("WordCard描画", isSaved)
+
+const speak = () => {
+    const utter = new SpeechSynthesisUtterance(word.word);
+    utter.lang = "en-UK";
+    speechSynthesis.speak(utter);
+    utter.text = word.word + " "; 
+    speechSynthesis.cancel();
+    utter.rate = 0.9
+};
+
 return (
 <div className="mb-2">
     <div className="mb-2">
@@ -17,11 +27,13 @@ return (
             <div className="mt-2 flex items-start justify-between ">
                 <div className="flex items-center gap-2 mb-2">
                     <h2 className="text-2xl font-bold">{word.word}</h2>
-                    <FaVolumeHigh color="grey" size={24} />
+                <button onClick={speak} className="text-gray-500 hover:text-gray-600 cursor-pointer transition-colors duration-150">
+                    <FaVolumeHigh size={24} />
+                </button>
                 </div>
                 <button
                     onClick={onSave}
-                    className="text-blue-500 hover:text-blue-600"
+                    className="text-blue-500 hover:text-blue-900 transition-colors duration-150"
                 >
                     {isSaved ? <BsBookmarkFill size={24} /> : <BsBookmark size={24} />}
                 </button>

90c0f104301c5590294479a5be88803dd59e63c1
takakowatabe <kikotkk@gmail.com>
2025-06-03 10:42:41 +0900

    進捗

---
+++ src/components/Layout.tsx
@@ -0,0 +1,28 @@
+import Header from '../components/Header'
+// import Sidebar from "../components/Sidebar";
+
+
+const Layout = ({
+    children,
+    sidebar,
+    }: {
+    children: React.ReactNode;
+    sidebar?: React.ReactNode;
+    }) => (
+
+
+    <>
+
+    <Header />
+    <div className=" w-full bg-gray-100 min-h-screen">
+        <main className="flex pt-8 flex-col md:flex-row gap-4 max-w-[800px] w-full mx-auto">
+            <section className="basis-4/5">{children}</section>
+            <aside className="basis-2/5">{sidebar}</aside>
+        </main>
+    </div>
+</>
+
+);
+
+
+export default Layout;
---
+++ src/pages/WordList.tsx
@@ -0,0 +1,11 @@
+const WordList = () => {
+return (
+<div>
+    <h1>Word List</h1>
+    <p>This is the word list page.</p>
+</div>
+)
+}
+;
+
+export default WordList;

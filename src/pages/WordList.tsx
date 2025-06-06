971fad58cd250b4281a04eefee4a79b95362d43e
takakowatabe <kikotkk@gmail.com>
2025-06-06 12:43:49 +0900

    文言修正

--- src/pages/WordList.tsx
+++ src/pages/WordList.tsx
@@ -18,7 +18,7 @@ const handleToggleSave = async (word: WordInfo) => {
    const currentWords = await fetchWordlists(); //supabaseからデータ取得
   //単語が保存済みかどうかをチェック
     const isSaved = currentWords.some(w => w.word === word.word); //保存済みの単語と同じ文字列があるか
-
+    
   //保存の上限設定
     if (!isSaved && currentWords.length >= 30) {
         toast.error("保存できる単語は30個までです");

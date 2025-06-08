32f002fa4ad72eb6cba7a8ac1cea71f1fd87d0a0
takakowatabe <kikotkk@gmail.com>
2025-06-08 10:34:57 +0900

    0608進捗

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,14 @@
-export default function Header() {
+import { Link } from "react-router-dom";
+
+
+const Header = () => {
     return (
         <header className="bg-white text-black py-2 px-8">
+        <Link to="/">
         <img src="/logo.svg" alt="logo" className="h-8 mr-3" />
+        </Link>
         </header>
+
     )
 }
+export default Header;

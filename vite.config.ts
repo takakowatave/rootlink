f47d0b541dac9bf577f8e8f009698ba18cef5509
takakowatave <kikotkk@gmail.com>
2025-09-26 14:23:33 +0900

    test

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 import env from "vite-plugin-env-compatible";
 
 export default defineConfig({
+    base: './',
     plugins: [react(), env({ prefix: "VITE", mountedPath: "process.env" })],
 });
-

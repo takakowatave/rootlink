db152254e1512dee595d9f80de4cc5610bc65185
takakowatave <kikotkk@gmail.com>
2025-10-23 12:05:00 +0900

    進捗

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,8 +1,6 @@
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.VITE_SUPABASE_URL as string
-const supabaseAnonKey = process.env.VITE_SUPABASE_ANON_KEY as string
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL as string;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY as string;
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
-
-
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
